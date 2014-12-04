e33d494514c54e148793016c9995983adeffdcef
Matthew Beatty <beattymg@gmail.com>
2014-12-03 19:42:37 -0500

    start of detail view with graph

--- Coursica/DetailViewController.h
+++ Coursica/DetailViewController.h
@@ -3,15 +3,26 @@
 //  Coursica
 //
 //  Created by Regan Bell on 11/25/14.
+//  Matthew Beatty was here.
 //  Copyright (c) 2014 Prestige Worldwide. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "GraphKit.h"
 
 @class Course;
 
 @interface DetailViewController : UIViewController
 
 @property (strong, nonatomic) Course *course;
+
+@property (nonatomic, strong) NSArray *data;
+@property (nonatomic, strong) NSArray *labels;
+
+- (IBAction)onButtonFill:(id)sender;
+
+- (IBAction)onButtonChange:(id)sender;
+
+- (IBAction)onButtonReset:(id)sender;
 
 @end
