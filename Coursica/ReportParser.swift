73b173d824d526dcd23153f2a5f994e66ce379f5
ReganBell <regan@seed.co>
2015-07-28 10:29:17 -0700

    Add dedicated parsers for model objects that get data from Firebase

---
+++ Coursica/ReportParser.swift
@@ -0,0 +1,34 @@
+//
+//  ReportParser.swift
+//  Coursica
+//
+//  Created by Regan Bell on 7/24/15.
+//  Copyright (c) 2015 Prestige Worldwide. All rights reserved.
+//
+
+import UIKit
+
+class ReportParser: NSObject {
+    class func reportFromSnapshot(snapshot: FDataSnapshot) -> Report? {
+        
+        if snapshot.value is NSNull {
+            return nil
+        } else {
+            let reportDictionary = snapshot.value.allValues.first! as! NSDictionary
+            let report = Report()
+            report.setFieldsWithList(["term", "year", "enrollment", "comments", "responses"], data: reportDictionary)
+            if let responses = reportDictionary["responses"] as? NSDictionary {
+                for (key, value) in responses {
+                    if let responseDict = (value as? NSDictionary) {
+                        if let question = key as? String {
+                            let response = ResponseParser.responseFromDictionary(responseDict)
+                            response.question = question
+                            report.responses.append(response)
+                        }
+                    }
+                }
+            }
+            return report
+        }
+    }
+}
