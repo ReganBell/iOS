f1d8adb55b281df62c950a3df3dfb296ad68dc7b
ReganBell <regan@seed.co>
2015-07-28 09:50:02 -0700

    move search to swift / realm

---
+++ Coursica/FastTokenizer.h
@@ -0,0 +1,15 @@
+//
+//  FastTokenizer.h
+//  Coursica
+//
+//  Created by Regan Bell on 7/14/15.
+//  Copyright (c) 2015 Prestige Worldwide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FastTokenizer : NSObject
+
++ (NSArray*)tokensFromString:(NSString*)field; 
+
+@end
